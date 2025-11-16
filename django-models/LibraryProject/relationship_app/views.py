172ddf15350b2517f46ec39b777e7cfc95ff25d1
ActualGEO <gbadamosioluwaobi10@gmai.com>
2025-11-16 10:31:55 +0100

    templates done

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Book, Library, Author
+from .models import Library
+from .models import Book, Author
 
 # Create your views here.
 def list_books(request):
