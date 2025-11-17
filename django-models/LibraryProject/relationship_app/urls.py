83e3f68f650f0b23bc12dd6254096263d79be145
ActualGEO <gbadamosioluwaobi10@gmai.com>
2025-11-17 07:35:55 +0100

    add my urls for app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
-from .views import list_books, LibraryDetailView, login_view, logout_view, register_view
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlspatterns = [
     path('books/', list_books, name='list_book'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='libary_view'),
-    path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'), 
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='libary_detail'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'), 
 
 
 ]
