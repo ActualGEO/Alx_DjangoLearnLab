539fe57f957473b3922e8b9b074e74f1403367ab
ActualGEO <gbadamosioluwaobi10@gmai.com>
2025-12-14 10:31:36 +0100

    updated my fliters again

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -9,9 +9,9 @@ from rest_framework.generics import (
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
-from rest_framework.filters import OrderingFilter
+from django_filters import rest_framework
+from rest_framework import filters
+from rest_framework import generics
 
 # Create your views here.
 class ListView(ListAPIView):
